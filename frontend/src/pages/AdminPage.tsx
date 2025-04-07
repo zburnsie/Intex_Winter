4d7e7da8d2e6adbb2a7f7c182e78236066580bed
ccstephenson <ccstephenson@gmail.com>
2025-04-07 10:42:37 -0600

    Added placeholder files in the api, components, types, and context folders

---
+++ frontend/src/pages/AdminPage.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+
+const AdminMoviePage = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [pageSize, setPageSize] = useState<number>(10); // Example page size for pagination
+    const [totalPages, setTotalPages] = useState<number>(0); // Total number of pages for pagination
+    const [showForm, setShowForm] = useState<boolean>(false); // State to control the visibility of the form for adding a new movie
+    const [editingMovie, setEditingMovie] = useState<Movie | null>(null); // State to track the movie being edited
+
+    useEffect(()) => {
+        const loadMovies = async () => {
+            try {
+                const data = await fetchMovies(pageSize, pageNum, []);
+                setMovies(data.movies);
+                setTotalPages(Math.ceil(data.totalNumMovies / pageSize))
+            } catch (error) {
+                setError((error as Error).message);
+            } finally {
+                setLoading(false); // Set loading to false after fetching
+            }
+        }
+    }
+
+
+    return (
+        <>
+        <div>
+            <h1>Admin Page</h1>
+            <p>This is the admin page for managing movies.</p>
+            {/* You can add more components or functionality here as needed */}
+        </div>
+        </>
+    );
+}
+
+export default AdminMoviePage;
