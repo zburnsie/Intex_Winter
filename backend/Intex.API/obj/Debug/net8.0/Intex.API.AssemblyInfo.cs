f76d099b37e8b2b08c170eb6d6cb51559a323271
ccstephenson <ccstephenson@gmail.com>
2025-04-07 10:16:53 -0600

    Initial commit

--- backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
+++ backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c82fd5fd2b945c8812ab5bf4dc23b48657f7a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
