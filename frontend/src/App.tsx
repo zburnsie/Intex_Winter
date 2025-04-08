00512cdf465ae7d279c9364816831426efb2fe19
lkehl247 <lkehl129@byu.edu>
2025-04-07 18:10:50 -0600

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import LandingPage from './pages/Landing';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-
-import ItemList from "./ItemList";
-import AdminMoviePage from "./pages/AdminMoviePage";
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ItemList from './ItemList';
+import AdminMoviePage from './pages/AdminMoviePage';
 import MoviesPage from './pages/MoviesPage';
-
 
 const App: React.FC = () => {
   return (
-
-    <Router>
-      <Routes>
+    <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<ItemList />} />
-        <Route path="/adminpage" element={<AdminMoviePage />} />
-        <Route path="/movies" element={<MoviesPage />} />
-        {/* You can add <Route path="/login" element={<LoginPage />} /> etc. later */}
-      </Routes>
-    </Router>
+      <Route path="/itemlist" element={<ItemList />} />
+      <Route path="/adminpage" element={<AdminMoviePage />} />
+      <Route path="/movies" element={<MoviesPage />} />
+    </Routes>
   );
 };
 
