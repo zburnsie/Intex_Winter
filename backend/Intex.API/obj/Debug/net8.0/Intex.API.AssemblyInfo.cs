cf3671cf1c46da722a4169e0b7fda9f52eab0fa4
lkehl247 <lkehl129@byu.edu>
2025-04-07 18:22:39 -0600

    trying without the movies

--- backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
+++ backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc8a78e4dce6abb118a7aec1de00d8a4afdf4d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00512cdf465ae7d279c9364816831426efb2fe19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
