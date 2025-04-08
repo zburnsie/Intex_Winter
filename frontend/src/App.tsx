d48f55fc2eaf1dabbdabcde5a1e746f734859929
lkehl247 <lkehl129@byu.edu>
2025-04-07 20:35:49 -0600

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/Landing';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import ItemList from './ItemList';
 import AdminMoviePage from './pages/AdminMoviePage';
 import MoviesPage from './pages/MoviesPage';
 
@@ -14,7 +13,6 @@ const App: React.FC = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/itemlist" element={<ItemList />} />
       <Route path="/adminpage" element={<AdminMoviePage />} />
       <Route path="/movies" element={<MoviesPage />} />
     </Routes>
