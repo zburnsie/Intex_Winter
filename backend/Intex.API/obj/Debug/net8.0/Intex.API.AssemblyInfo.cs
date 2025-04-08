0d97913cd183f775ac528117daca23a755b53b70
lkehl247 <lkehl129@byu.edu>
2025-04-07 19:26:24 -0600

    Update Intex.API.AssemblyInfo.cs

--- backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
+++ backend/Intex.API/obj/Debug/net8.0/Intex.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00512cdf465ae7d279c9364816831426efb2fe19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec7fd6d03c17087036033d04a882c4e63e254f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
