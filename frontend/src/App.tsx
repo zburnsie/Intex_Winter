378ceeca0ce769983235de09a0c2464b0ef38d36
Ryan Pace <ryanjp2@byu.edu>
2025-04-07 20:38:13 -0600

    fixed app.tsx browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import LandingPage from './pages/Landing';
 import LoginPage from './pages/LoginPage';
@@ -9,28 +9,26 @@ import MoviesPage from './pages/MoviesPage';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        {/* Optional Navigation Links - good for dev testing */}
-        <nav style={{ padding: '1rem', background: '#eee', marginBottom: '1rem', display: 'flex', gap: '1rem' }}>
-          <Link to="/">Home</Link>
-          <Link to="/adminpage">Admin Movie Page</Link>
-          <Link to="/movies">Browse Movies</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-          <Link to="/itemlist">Item List</Link>
-        </nav>
+    <div>
+      {/* Optional Navigation Links - good for dev testing */}
+      <nav style={{ padding: '1rem', background: '#eee', marginBottom: '1rem', display: 'flex', gap: '1rem' }}>
+        <Link to="/">Home</Link>
+        <Link to="/adminpage">Admin Movie Page</Link>
+        <Link to="/movies">Browse Movies</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/adminpage" element={<AdminMoviePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/adminpage" element={<AdminMoviePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </div>
   );
 };
 
 export default App;
+
