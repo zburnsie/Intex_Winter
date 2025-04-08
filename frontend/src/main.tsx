f4cd73828534ffdc39a8a3f041461c3342b6c44c
Ryan Pace <ryanjp2@byu.edu>
2025-04-07 19:09:05 -0600

    Got MoviePage to populate with images, and made the titles tooltips rather than labels

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ Place this near the top
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
