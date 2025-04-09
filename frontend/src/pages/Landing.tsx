633b930a9da3908e140b6ff88d073d490bc35609
zburnsie <zburnsie@byu.edu>
2025-04-09 13:06:07 -0600

    Remove the unused stuff

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -36,25 +36,25 @@ const LandingPage = () => {
       .trim();
   };
 
-  const shuffleArray = (array: any[]) => {
-    let currentIndex = array.length,
-      randomIndex;
+  // const shuffleArray = (array: any[]) => {
+  //   let currentIndex = array.length,
+  //     randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex !== 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+  //   // While there remain elements to shuffle...
+  //   while (currentIndex !== 0) {
+  //     // Pick a remaining element...
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex--;
 
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
+  //     // And swap it with the current element.
+  //     [array[currentIndex], array[randomIndex]] = [
+  //       array[randomIndex],
+  //       array[currentIndex],
+  //     ];
+  //   }
 
-    return array;
-  };
+  //   return array;
+  // };
 
   useEffect(() => {
     const fetchPosters = async () => {
