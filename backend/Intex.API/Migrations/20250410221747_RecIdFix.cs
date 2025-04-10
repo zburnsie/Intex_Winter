361d137020457bd794c5a5c5a375b44b0e624096
zburnsie <zburnsie@byu.edu>
2025-04-10 16:55:46 -0600

    added icons on admin, fixed the rec_id it's all workin

---
+++ backend/Intex.API/Migrations/20250410221747_RecIdFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intex.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecIdFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rec_Id",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rec_Id",
+                table: "AspNetUsers");
+        }
+    }
+}
